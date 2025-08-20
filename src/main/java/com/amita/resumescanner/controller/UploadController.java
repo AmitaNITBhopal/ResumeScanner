22a80ccc05c27245a61178d1cea0e870b4c6dfad
AmitaNITBhopal <amita.nitbhopal@gmail.com>
2025-08-20 12:06:02 +0200

    Defined path of uploaded files in application.properties files

--- src/main/java/com/amita/resumescanner/controller/UploadController.java
+++ src/main/java/com/amita/resumescanner/controller/UploadController.java
@@ -2,6 +2,7 @@ package com.amita.resumescanner.controller;
 
 import java.io.File;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +14,20 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
 @Validated
 public class UploadController {
+	
+	@Value("${uploadPath}")
+	private String uploadPath;
 
 	@PostMapping("/upload")
 	public ResponseEntity<?> handleFileUpload( @RequestParam("file") MultipartFile file ) {
 
 	    String fileName = file.getOriginalFilename();
 	    try {
-	      file.transferTo( new File("C:\\upload\\" + fileName));
+	    	String uploadFileName = uploadPath + fileName;
+	    	System.out.println("File name: " + uploadFileName);
+	      file.transferTo( new File(uploadFileName));
 	    } catch (Exception e) {
 	      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 	    } 
