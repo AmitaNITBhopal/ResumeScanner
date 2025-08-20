ceba1dfa264fe6ce3d86239e6a0899c27af69548
AmitaNITBhopal <amita.nitbhopal@gmail.com>
2025-08-20 07:03:44 +0200

    Added new repository

---
+++ src/test/java/com/amita/resumescanner/ResumescannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amita.resumescanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumescannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
